d7312963df3b6b9241b4d1ec5766c0a9ac199004
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-12-22 15:57:18 +0400

    Example of datasource query filter

--- modules/web/src/sample/web/orders/customer/CustomerBrowse.java
+++ modules/web/src/sample/web/orders/customer/CustomerBrowse.java
@@ -1,12 +1,18 @@
-/*
- * TODO Copyright
- */
 package sample.web.orders.customer;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import sample.entity.orders.Customer;
 
-/**
- * @author knst
- */
+import javax.inject.Inject;
+import java.util.UUID;
+
 public class CustomerBrowse extends AbstractLookup {
+
+    @Inject
+    private CollectionDatasource<Customer, UUID> customersDs;
+
+    public void applyFilter() {
+        customersDs.refresh();
+    }
 }
